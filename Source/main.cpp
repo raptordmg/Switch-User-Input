e9ea75f34a8e908d02f612cac1e27de306c61fab
raptordmg <raptordmg@gmail.com>
2018-11-11 12:01:47 +0000

    Add files via upload

---
+++ Source/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include "switch.h"
+using namespace std;
+
+int main()
+{
+	consoleInit(NULL);
+	string msg = "Hello World!";
+	int num1 = 0;
+	u32 kDownOld = 0;
+	cout << msg << endl << num1;
+	while (appletMainLoop())
+	{
+		hidScanInput();
+		u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+		if (kDown != kDownOld)
+		{
+			if (kDown & KEY_PLUS) break;
+			if (kDown & KEY_UP)
+			{
+				num1 = num1 + 1;
+				consoleClear();
+				cout << msg << endl << num1;
+			}
+			else if (kDown & KEY_DOWN)
+			{
+				num1 = num1 - 1;
+				consoleClear();
+				cout << msg << endl << num1;
+			}
+		}
+		kDownOld = kDown;
+		consoleUpdate(NULL);
+	}
+	consoleExit(NULL);
+	return 0;
+}
